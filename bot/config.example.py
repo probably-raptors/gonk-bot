82c62e2729da6790f99dface6c7edf49c4048e29
Bryan Altman <brynaltman@gmail.com>
2021-01-19 12:20:15 +0200

    re-aligning config.py across branches

--- bot/config.example.py
+++ bot/config.example.py
@@ -1,26 +1,15 @@
 # Config usage:
-#   Copy this file and remove .example, 'cp config.example.py config.py'
-#   See Mike or Bryan for any issues or questions.
+#   Copy this file and remove .example ==> 'cp config.example.py config.py'
 
-# TOKENS:
-#    Get tokens from discord dev portal/server, add them to
-#    DISCORD_TOKEN and DISCORD_GUILD
-
-import os
-
-def init():
-    global prefix, admin_roles, default_role, token, guild_id
-
+CONFIG = {
     # change this string to your preferred command prefix
-    prefix = "."
+    'prefix': ".",
 
     # change these strings to your prefered admin and default roles
-    admin_roles = ["Black Giant Squirrel", "Grizzled Squirrel"]
-    default_role = "Kit"
+    'ADMIN_ROLES' : ["Black Giant Squirrel", "Grizzled Squirrel"],
+    'DEFAULT_ROLE' : "Kit",
 
-    # leave these alone
-    DISCORD_TOKEN = "REPLACE ME"
-    token = DISCORD_TOKEN
-
-    DISCORD_GUILD = "REPLACE ME"
-    guild_id = DISCORD_GUILD
+    # Get tokens from discord dev portal/server, add them to
+    'DISCORD_TOKEN' : "REPLACE ME",
+    'DISCORD_GUILD' : "REPLACE ME"
+}
