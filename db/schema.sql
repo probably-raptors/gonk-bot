22c0f2a3504410eef2da56b58dace3f715334e64
odonnellm <modonnell@surveylaunch.com>
2021-04-07 16:48:39 -0400

    Schema update

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS gonk_bot;
 
 use gonk_bot;
 
+DROP TABLE IF EXISTS watch_cog;
 CREATE TABLE IF NOT EXISTS watch_cog(
        id INT PRIMARY KEY AUTO_INCREMENT,
        symbol VARCHAR(20),
