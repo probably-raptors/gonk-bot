7ebb3afeeb97d4b64793f0e22b76e8fc0c112b8a
odonnellm <modonnell@surveylaunch.com>
2021-04-13 16:05:13 -0400

    more config cleanup, just removing stuff that shouldn't be shown in a public code base

--- bot/config.example.py
+++ bot/config.example.py
@@ -4,29 +4,29 @@
 
 # TOKENS:
 #    Get tokens from discord dev portal/server, add them to
-#    DISCORD_TOKEN and DISCORD_GUILD
+#    DISCORD_TOKEN
 
 import os
 
 CONFIG = {
     # change this string to your preferred command prefix
-    'PREFIX': ".",
+    'PREFIX': '.',
 
     # change these strings to your prefered admin and default roles
     'ADMIN_ROLES' : [],
-    'DEFAULT_ROLE': "",
+    'DEFAULT_ROLE': '',
 
     # Get tokens from discord dev portal/server, add them to
-    'DISCORD_TOKEN' : "REPLACE ME",
+    'DISCORD_TOKEN' : '',
 
     # DB config options, unless your dev environment is different, leave these alone
-    'DBNAME': 'gonk_bot',
-    'DBUSER': 'root',
+    'DBNAME': '',
+    'DBUSER': '',
     'DBPASS': '',
     'DBHOST': '',
 
     # API Keys
-    'CMCKEY': 'REPLACE ME',
+    'CMCKEY': '', # COINMARKETCAP
 
     # Sarcasm Cog banned channels
     'SARCASM_BAN': [
