5e12eef7d677f5481eaeef58b2e50bef3daec3ba
odonnellm <modonnell@surveylaunch.com>
2021-04-19 11:33:28 -0400

    Fixed bug where bot was only responding with first char of message in sarcasm module

--- bot/modules/sarcasm/cog.py
+++ bot/modules/sarcasm/cog.py
@@ -7,15 +7,13 @@ class SarcasmCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    def sarcastify(self, msg: str):
-        new_msg = ""
-        cap = False
+    def sarcastify(self, msg):
+        new_msg = ''
         for i, c in enumerate(msg):
-            if i % 2:
-                new_msg += c.upper()
-            else:
-                new_msg += c.lower()
-                return new_msg
+            if i % 2: new_msg += c.upper()
+            else:     new_msg += c.lower()
+
+        return new_msg
 
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message):
@@ -24,8 +22,7 @@ class SarcasmCog(commands.Cog):
             return
 
         # change the range to (1, 1) for testing / trolling
-        r = random.randint(1, 100)
-        if r == 1:
+        if random.randint(1, 100) == 1:
             new_msg = self.sarcastify(msg.content)
             await msg.channel.send(new_msg)
 
